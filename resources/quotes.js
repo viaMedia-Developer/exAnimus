3fb144a27e39c5002fab133ac24aa9a4259b6f8b
viaMedia-Developer <cpsavante@gmail.com>
2017-09-05 20:14:59 -0400

    1.0 Initial Commit: Inspiration section functionality operational, functions working as intended

---
+++ resources/quotes.js
@@ -0,0 +1,98 @@
+var qte = {
+	"quotes": [
+		{
+			number: "I",
+			quote: "Look for an opportunity to give, and an opportunity to receive shall always find you."
+		},
+		{
+			number: "II",
+			quote: "Start looking at what you have and be grateful for it; instead of looking at what's out there, wishing you had it."
+		},
+		{
+			number: "III",
+			quote: "Find that in life that you look forward to, that it makes everything else easier to deal with."
+		},
+		{
+			number: "IV",
+			quote: "Let passion pull you through your path, steer with a clear mind"
+		},
+		{
+			number: "V",
+			quote: "You can do anything, go anywhere and obtain anything as long as you're willing to believe that there is a way."
+		},
+		{
+			number: "VI",
+			quote: "It doesn't matter what you say but what you believe. Yet, you can change what to believe with what you say."
+		},
+		{
+			number: "VII",
+			quote: "The most challenging part about starting something is starting something."
+		},
+		{
+			number: "VIII",
+			quote: "You move up in the world from the things that you do and give, not the things you get."
+		},
+		{
+			number: "IX",
+			quote: "Big words don't allot you as much sophistication as there source does. An extensive vocabulary means little if you don't have the knowledge and experience to back it up."
+		},
+		{
+			number: "X",
+			quote: "Live life: you don't have to earn it"
+		},
+		{
+			number: "XI",
+			quote: "You have to accept reality before you can make adjustments to it"
+		},
+		{
+			number: "XII",
+			quote: "One of the most powerful and effective ways to create your future is to vividly envision it as if you are currently living it."
+		},
+		{
+			number: "XIII",
+			quote: "Look forward to the future. Persist in the present"
+		},
+		{
+			number: "XIV",
+			quote: "To make money, go where the money is. To be happy, be where the joy lies. To have both, tread lines where they vie. "
+		},
+		{
+			number: "XV",
+			quote: "Always start with what you have and what you can do. "
+		},
+		{
+			number: "XVI",
+			quote: "Know what it is you would like. Allow it to be. Then let it go."
+		},
+		{
+			number: "XVII",
+			quote: "If you're learning to fly, you don't live in a cage"
+		},
+		{
+			number: "XVIII",
+			quote: "Experiences aren't to define you: they are to help reveal to yourself who you truly are, and perhaps who you can be"
+		},
+		{
+			number: "XIX",
+			quote: "Let the fact that you're still alive be motivation for you to prosper"
+		},
+		{
+			number: "XX",
+			quote: "An idea is just oil: until you put a spark to it"
+		},
+		{
+			number: "XXI",
+			quote: "Spreading positivity is how you foster positivity"
+		},
+		{
+			number: "XXII",
+			quote: "Things come and go, but the impact from an experience can last a lifetime"
+		},
+	]
+}
+
+
+		// {
+		// 	number: "",
+		// 	quote: ""
+		// },
---
+++ resources/script.js
@@ -0,0 +1,108 @@
+var quotes = qte.quotes;
+
+var navLinks = document.querySelectorAll('nav#main a');
+
+var getSiblings = (elem) => {
+	var siblings = [];
+	var sibling = elem.parentNode.firstChild;
+	for (; sibling; sibling = sibling.nextSibling) {
+		if(sibling.nodeType !== 1 || sibling == elem) continue;
+		siblings.push(sibling);
+	}
+	return siblings;
+}
+
+navLinks.forEach((index) => {
+	var currentSlide = document.querySelector(index.getAttribute('href'));
+	index.addEventListener('click', function(e) {
+		e.preventDefault();
+
+		var linkSibling = getSiblings(this);
+		(index.classList.contains('active') === true) ? index.classList.remove('active') : index.classList.add('active');
+		linkSibling[0].classList.remove('active');
+
+		currentSlide.classList.add('active');
+		var siblings = getSiblings(currentSlide);
+		siblings[0].classList.remove('active');
+	});
+});
+
+
+
+
+
+const preceding = document.getElementById('preceding'),
+	succeeding = document.getElementById('succeeding'),
+	randomButton = document.getElementById('random');
+
+var	quoteHeader = document.querySelector('#quote h1'),
+	quoteText = document.querySelector('#quote p'),
+
+	pickRandom = () => { 
+		number = Math.round(Math.random() * quotes.length);
+		return number;
+	},
+	random = pickRandom(),
+	current;
+ 	
+
+var loadQuote = () => {
+	quoteHeader.innerText = quotes[random].number;
+	quoteText.innerText = quotes[random].quote;
+	current = random;
+}; 
+window.onload = () => { loadQuote(); }
+
+var randomQuote = () => {
+	fade(quoteHeader); fade(quoteText);
+	current = pickRandom();
+	setTimeout(function() {
+		quoteHeader.innerText = quotes[current].number;
+		quoteText.innerText = quotes[current].quote;
+	}, 800);
+	setTimeout(function() {
+		appear(quoteHeader); appear(quoteText);
+	}, 1000);
+}
+
+var nextQuote = () => {
+	fade(quoteHeader); fade(quoteText);
+	current++;
+	if (current > (quotes.length - 1)) {
+		current = 0;
+	}
+	setTimeout(function() {
+		quoteHeader.innerText = quotes[current].number;
+		quoteText.innerText = quotes[current].quote;
+	}, 800);
+	setTimeout(function() {
+		appear(quoteHeader); appear(quoteText);
+	}, 1000);
+	
+}
+
+var prevQuote = () => {
+	fade(quoteHeader); fade(quoteText);
+	current--;
+	if (current <= 0) {
+		current = 0;
+	}
+	setTimeout(function() {
+		quoteHeader.innerText = quotes[current].number;
+		quoteText.innerText = quotes[current].quote;
+	}, 800);
+	setTimeout(function() {
+		appear(quoteHeader); appear(quoteText);
+	}, 1000);
+}
+
+var fade = (element) => {	
+	(element.classList.contains('appear') === true) ? element.classList.remove('appear') & element.classList.add('fade') : element.classList.add('fade');
+}
+var appear = (element) => {
+	(element.classList.contains('fade') === true) ? element.classList.remove('fade') & element.classList.add('appear') : element.classList.add('appear');
+}
+
+preceding.addEventListener('click', prevQuote);
+succeeding.addEventListener('click', nextQuote);
+randomButton.addEventListener('click', randomQuote);
